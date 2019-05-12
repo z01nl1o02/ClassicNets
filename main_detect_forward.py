1f35563d5555422b15f48ecb9f4a50b0e31776fe
z01nl1o02 <z0n1l@outlook.com>
2019-05-12 11:20:12 +0800

    nop

--- main_detect_forward.py
+++ main_detect_forward.py
@@ -5,25 +5,34 @@ from mxnet import lr_scheduler,nd
 from datasets import detect_voc
 from networks import ssd
 from utils import predict_ssd
-import os,pdb,cv2
+import os,pdb,cv2,sys
 from tools.eval_metric import VOC07MApMetric
 
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(level = logging.INFO)
+
 handler = logging.FileHandler("forward_log.txt")
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
+
+
+scr_handler = logging.StreamHandler()
+scr_handler.setLevel(logging.INFO) 
+scr_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(message)s"))
+
+
 logger.addHandler(handler)
+logger.addHandler(scr_handler)
 
 ctx = mx.gpu(0)
 
 
 
 
-testset = detect_voc.DETECT_VOC("test","2007",False)
+testset = detect_voc.DETECT_VOC("trainval","2007",False)
 classes = testset._classes
 number_classes = len(classes)
 
@@ -36,7 +45,7 @@ net.collect_params().reset_ctx(ctx)
 
 logger.info("========ssd forward===========")
 mAP = VOC07MApMetric()
-thresh = 0.2
+thresh = 0.0
 
 for idx in range(len(testset)):
     feat,target = testset[idx]
@@ -49,10 +58,10 @@ for idx in range(len(testset)):
     H,W,C = src.shape
     for one in output:
         cls,score,x0,y0,x1,y1 = one * np.array([1,1,W,H,W,H])
-	x0,x1,y0,y1 = [int(x) for x in [x0,x1,y0,y1]]
-	info = "{}_{:.3f}".format(classes[int(cls)],score)
-	cv2.putText(src,info,(x0,y0),cv2.FONT_HERSHEY_COMPLEX,1.0,(255,255,0))
-	cv2.rectangle(src,(x0,y0),(x1,y1),(255,0,0),2)
+        x0,x1,y0,y1 = [int(x) for x in [x0,x1,y0,y1]]
+        info = "{}_{:.3f}".format(classes[int(cls)],score)
+        cv2.putText(src,info,(x0,y0),cv2.FONT_HERSHEY_COMPLEX,1.0,(255,255,0))
+        cv2.rectangle(src,(x0,y0),(x1,y1),(255,0,0),2)
     cv2.imwrite(os.path.join('debug',name),src)
     labels,preds = np.expand_dims(target,0), np.expand_dims(output,0)
     labels,preds = np.expand_dims(labels,0), np.expand_dims(preds,0)
@@ -60,7 +69,7 @@ for idx in range(len(testset)):
     mAP.update(labels,preds)
     if idx > 0 and 0 == (idx % 100):
         logger.info(mAP.get())
-	#break
+	break
 
 logger.info("in total:")
 logger.info(mAP.get())        
