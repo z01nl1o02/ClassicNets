d2267d357f81cabd9ebc3538d5309b80f12dfddf
cqs <cqs@cqs>
2019-07-14 01:53:52 +0800

    support cub200

---
+++ datasets/cub200.py
@@ -0,0 +1,92 @@
+import mxnet as mx
+from mxnet import gluon
+import cv2,random,os
+import numpy as np
+
+class CUB200(mx.gluon.data.Dataset):
+    def __init__(self,fortrain,root=None,resize=None):
+        super(CUB200,self).__init__()
+        if root is None:
+            root = os.path.join(os.getenv("ENV_DATASET_DIR"), "CUB_200_2011")
+        images = []
+        with open(os.path.join(root,"images.txt"),'r') as f:
+            for line in f:
+                image_id,relative_path = line.strip().split(' ')
+                images.append((relative_path,int(image_id)))
+        classes = []
+        with open(os.path.join(root,"classes.txt"),'r') as f:
+            for line in f:
+                class_id, class_name = line.strip().split(' ')
+                classes.append((class_name, int(class_id)-1))
+        classes = dict(classes)
+
+        flags = []
+        with open(os.path.join(root,"train_test_split.txt"),'r') as f:
+            for line in f:
+                image_id, flag = [int(x) for x in line.strip().split(' ')]
+                if not fortrain:
+                    flag = 1 - flag
+                flags.append( (image_id, flag)   )
+        flags = dict(flags)
+
+        self._mean = np.array([0.4914, 0.4822, 0.4465])
+        self._std = np.array([0.2023, 0.1994, 0.2010])
+
+        self._fortrain = fortrain
+        self.class_names = classes.keys()
+        self.data_list = []
+        for path,image_id in images:
+            class_name = path.strip().split('/')[0]
+            class_id = classes[class_name]
+            if flags[image_id]:
+                self.data_list.append((os.path.join(root,"images",path),  class_id))
+        return
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self,idx):
+        image = cv2.imread(self.data_list[idx][0],1)
+        label = int(float(self.data_list[idx][1]))
+        image = np.float32(image) / 255.0
+        image = mx.nd.array(image)
+
+        data_shape = (3,244,244)
+        if self._fortrain:
+            augs = mx.image.CreateAugmenter(data_shape=data_shape, resize=244, rand_mirror=True, rand_crop=True,
+                                         brightness = 0.5,contrast = 0.5, saturation  = 0.5, hue = 0.5,
+                                         mean=self._mean, std=self._std)
+        else:
+            augs = mx.image.CreateAugmenter(data_shape=data_shape, resize=244, rand_mirror=False, rand_crop=False,
+                                         mean=self._mean, std=self._std)
+
+        for aug in augs:
+            image = aug(image)
+
+        if 0:
+            vis = image.asnumpy() * self._std + self._mean
+            vis = vis * 255
+            print(vis.max(), vis.min())
+            vis = np.clip(vis,0,255).astype(np.uint8)
+            cv2.imshow("vis",vis)
+            cv2.waitKey(-1)
+
+        image = mx.nd.transpose(image,(2,0,1))
+        return image,label
+
+    def get_class_names(self):
+        return self.class_names
+
+def load(batch_size):
+    trainset = CUB200(True)
+    testset = CUB200(False)
+    train_iter = gluon.data.DataLoader(trainset,batch_size,shuffle=True,last_batch="rollover",num_workers=-2)
+    test_iter = gluon.data.DataLoader(testset,batch_size,shuffle=False,last_batch="rollover",num_workers=-2)
+    print('cub200: train {} test {}'.format(len(trainset), len(testset)))
+    return train_iter, test_iter, trainset.get_class_names()
+
+if 0:
+    train_iter, _, names = load(10)
+    print(names)
+    for Y,X in train_iter:
+        print(Y.shape, X.shape)
--- main_classify.py
+++ main_classify.py
@@ -1,7 +1,7 @@
 import mxnet as mx
 from mxnet.gluon import Trainer
 from mxnet import lr_scheduler
-from datasets import fasionmnist,classify_dataset,cifar
+from datasets import fasionmnist,classify_dataset,cifar,cub200
 from networks import alexnet,vgg,nin,googlelenet,resnet,densenet,squeezenet
 from utils import train_net
 from tools import lr_schs
@@ -40,6 +40,26 @@ class RESNET_CIFAR10_CONFIG(NET_DATASET_CONFIG):
         return self._batch_size
 
 
+class RESNET_CUB200_CONFIG(NET_DATASET_CONFIG):
+    def __init__(self):
+        self._num_epochs = 150
+        self._batch_size = 64//4
+        self._base_lr = 0.1
+    def trainer(self,net):
+        print("trainer \t optim:sgd wd: 1e-4, momentum:0.9")
+        return Trainer(net.collect_params(),optimizer="sgd",optimizer_params={"wd":1e-4,"momentum":0.9})
+    def learning_rate_scheduler(self):
+        print("learning_rate \t base_lr:%.4f, scheduler: MultiFactorScheduler"%self._base_lr)
+        lr_sch = lr_scheduler.MultiFactorScheduler(step=[int(self._num_epochs * 0.5), int(self._num_epochs * 0.75) ], factor=0.1)
+        lr_sch.base_lr = self._base_lr
+        return lr_sch
+    def epoch_num(self):
+        print("epoch: %d"%self._num_epochs)
+        return self._num_epochs
+    def batch_size(self):
+        print('batch size: %d'%self._batch_size)
+        return self._batch_size
+
 class VGG_CIFAR10_CONFIG(NET_DATASET_CONFIG):
     def __init__(self):
         self._num_epochs = 100
@@ -66,16 +86,19 @@ class VGG_CIFAR10_CONFIG(NET_DATASET_CONFIG):
         return self._batch_size
 
 
-resnet_cifar10_config = RESNET_CIFAR10_CONFIG()
-vgg_cifar10_config = VGG_CIFAR10_CONFIG()
+configs = {
+    "vgg-11,cifar":VGG_CIFAR10_CONFIG(),
+    "resnet,cifar":RESNET_CIFAR10_CONFIG(),
+    "resnet,cub200":RESNET_CUB200_CONFIG()
+}
+
 
 if __name__=="__main__":
-
+    config_name = "resnet,cub200"
     ctx = mx.gpu(0)
-    batch_size = vgg_cifar10_config.batch_size()
-    num_epochs = vgg_cifar10_config.epoch_num()
-    net_name = "vgg-11"
-    data_name = "cifar"
+    batch_size = configs[config_name].batch_size()
+    num_epochs = configs[config_name].epoch_num()
+    net_name, data_name = config_name.split(',')
     output_folder = os.path.join("output")
     output_prefix = os.path.join(output_folder,net_name)
 
@@ -99,6 +122,8 @@ if __name__=="__main__":
             train_iter,test_iter,class_names = cifar.load(batch_size,None)
         else:
             train_iter,test_iter,class_names = cifar.load(batch_size)
+    elif data_name == "cub200":
+        train_iter,test_iter,class_names = cub200.load(batch_size)
     else:
         rec_train,rec_test = "fortrain.rec", "fortest.rec"  
         train_iter = mx.io.ImageRecordIter( path_imgrec = rec_train, data_shape = (3,32,32), batch_size = batch_size )
@@ -121,14 +146,12 @@ if __name__=="__main__":
         net = googlelenet.load(len(class_names))
         base_lr = 0.1
         resize=(96,96)
-    elif net_name == "resnet-N":
+    elif (net_name == "resnet-N" or net_name == "resnet"):
         net = resnet.load('resnet-N',len(class_names))
     elif net_name == "resnet-164":
         net = resnet.load('resnet-164',len(class_names))
     elif net_name == "densenet":
         net = densenet.load(len(class_names))
-        base_lr = 0.1
-        resize=(96,96)
     elif net_name == "squeezenet":
         net = squeezenet.load(len(class_names),(96,3,1,1))
         base_lr = 0.01 #must be with small lr
@@ -146,8 +169,8 @@ if __name__=="__main__":
 
 
     net.collect_params().reset_ctx(ctx)
-    trainer = vgg_cifar10_config.trainer(net)
-    lr_sch = vgg_cifar10_config.learning_rate_scheduler()
+    trainer = configs[config_name].trainer(net)
+    lr_sch = configs[config_name].learning_rate_scheduler()
 
 
 
