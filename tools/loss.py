d7b628771f3b8169f959513b01077c2c6560cacb
cqs <cqs@cqs>
2019-08-11 00:02:38 +0800

    nop

--- tools/loss.py
+++ tools/loss.py
@@ -17,10 +17,10 @@ class LabelSmoothSoftmaxCELoss(gluon.Block):
     def forward(self, pred, label):
         F = mx.nd
         K = pred.shape[-1]
-        pred_logist = F.log_softmax(pred)
-        pred_nllLoss = -1*F.log(pred_logist)
-        A = pred_nllLoss * self.smooth / (K-1)
-        B = F.pick(pred_nllLoss, label) * (1-self.smooth - self.smooth / (K-1))
+        loss = -F.log_softmax(pred)
+        A = loss * self.smooth / (K-1)
+        B = F.pick(loss, label) * (1-self.smooth - self.smooth / (K-1))
         return F.mean(A, axis=0, exclude=True) + F.mean(B, axis=0,exclude=True)
+
 
 
