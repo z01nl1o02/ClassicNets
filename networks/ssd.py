4cb829f229d500d78608d3f5b8bf2f99b2da2f1e
z01nl1o02 <z0n1l@outlook.com>
2019-09-15 02:10:51 +0800

    move nms into network and adjust lr

--- networks/ssd.py
+++ networks/ssd.py
@@ -59,11 +59,11 @@ def bbox_predictor(num_anchors):
 #下面的函数把(batch,C,H,W)转换成(batch,HxWxC)
 #最后在dim=1上连接
 #注意mx.nd.flatten()的功能和numpy.flatten()不同，mx.nd.flatten()会保留维度0，只合并后面的维度
-def flatten_pred(pred):
-    return pred.transpose((0, 2, 3, 1)).flatten()
+def flatten_pred(F,pred):
+    return F.flatten( F.transpose(pred,(0, 2, 3, 1)) )
 
 def concat_preds(F,preds):
-    return F.concat(*[flatten_pred(p) for p in preds], dim=1)    
+    return F.concat(*[flatten_pred(F,p) for p in preds], dim=1)
 
 class BackboneResnet34(nn.Block):
     def __init__(self):
@@ -218,7 +218,56 @@ class SSD_CUSTOM(nn.Block):
                 concat_preds(bbox_preds))
 #############################################################################
 #############################################################################
+import gluoncv as gcv
+class Postpreocess(gluon.HybridBlock):
+    def __init__(self,num_classes,nms_thresh=0.45, nms_topk=400, post_nms=100):
+        super(Postpreocess,self).__init__()
+        self.num_classes = num_classes
+        self.nms_thresh = nms_thresh
+        self.nms_topk = nms_topk
+        self.post_nms = post_nms
+        self.bbox_decoder = gcv.nn.coder.NormalizedBoxCenterDecoder(stds=[0.1,0.1,0.2,0.2])
+        self.cls_decoder = gcv.nn.coder.MultiPerClassDecoder(self.num_classes + 1, thresh=0.01)
+        self.corner2center = gcv.nn.bbox.BBoxCornerToCenter(split=False)
+        return
 
+
+    def hybrid_forward(self, F, anchors, pred_classes, pred_bboxes):
+        """
+        :param anchors: (1, num-of-anchor, 4), anchors[0,0,:] = x0,y0,x1,y1
+        :param pred_classes: (batch-size, num-of-anchor, num-of-classes), including background
+        :param pred_bboxes: (batch-size, num-of-anchor * 4)
+
+        :param ids: (batch-size, num-of-found, 1)  class id for each found
+        :param scores: (batch-size, num-of-found, 1)  class score for each found
+        :param bboxes: (batch-size, num-of-found, 4)  coordinates of each found  (x0,y0,x1,y1) with norm w/h
+        """
+        anchors = self.corner2center(anchors)
+        pred_bboxes = F.reshape(pred_bboxes, (0,-1,4))
+        bboxes = self.bbox_decoder(pred_bboxes, anchors)
+
+        cls_ids, scores = self.cls_decoder(F.softmax(pred_classes,axis=-1))
+
+
+        results = []
+        for i in range(self.num_classes):
+            cls_id = cls_ids.slice_axis(axis=-1, begin=i, end=i + 1)
+            score = scores.slice_axis(axis=-1, begin=i, end=i + 1)
+            # per class results
+            per_result = F.concat(*[cls_id, score, bboxes], dim=-1)
+            results.append(per_result)
+        result = F.concat(*results, dim=1)
+        if self.nms_thresh > 0 and self.nms_thresh < 1:
+            result = F.contrib.box_nms(
+                result, overlap_thresh=self.nms_thresh, topk=self.nms_topk, valid_thresh=0.01,
+                id_index=0, score_index=1, coord_start=2, force_suppress=False)
+            if self.post_nms > 0:
+                result = result.slice_axis(axis=1, begin=0, end=self.post_nms)
+
+        ids = F.slice_axis(result, axis=2, begin=0, end=1)
+        scores = F.slice_axis(result, axis=2, begin=1, end=2)
+        bboxes = F.slice_axis(result, axis=2, begin=2, end=6)
+        return ids, scores, bboxes
 
 class SSD(nn.HybridBlock):
     def __init__(self, num_classes, anchor_sizes=None, anchor_ratios=None, backbone=None, **kwargs):
@@ -245,7 +294,7 @@ class SSD(nn.HybridBlock):
 
         backbone = get_vgg16_300()
         print("using pretrained backbone: vgg16_atrous_300")
-        backbone.collect_params().setattr("lr_mult", 0.1)
+        backbone.collect_params().setattr("lr_mult", 0.4)
 
         self.backbone = backbone
         with self.stage_0.name_scope():
@@ -270,40 +319,46 @@ class SSD(nn.HybridBlock):
         self.stage_4.initialize(init=mx.initializer.Xavier())
         self.stage_5.initialize(init=mx.initializer.Xavier())
 
+        self.postprocess = Postpreocess(self.num_classes)
+
         return
 
     def hybrid_forward(self, F, X):
-        anchors, cls_preds, bbox_preds = [None] * 6, [None] * 6, [None] * 6
+        anchors, cls_preds, bbox_pred = [None] * 6, [None] * 6, [None] * 6
         # print(X.shape)
         Xs = self.backbone(X)
-        anchors[0], cls_preds[0], bbox_preds[0] = ssd_forward_one(Xs[0], self.anchor_sizes[0],
+        anchors[0], cls_preds[0], bbox_pred[0] = ssd_forward_one(Xs[0], self.anchor_sizes[0],
                                                                  self.anchor_ratios[0], self.stage_0[0],
                                                                  self.stage_0[1])
        # print(anchors[0].shape, cls_preds[0].shape, bbox_preds[0].shape)
-        anchors[1], cls_preds[1], bbox_preds[1] = ssd_forward_one(Xs[1], self.anchor_sizes[1],
+        anchors[1], cls_preds[1], bbox_pred[1] = ssd_forward_one(Xs[1], self.anchor_sizes[1],
                                                                  self.anchor_ratios[1], self.stage_1[0],
                                                                  self.stage_1[1])
         # print(X.shape)
-        anchors[2], cls_preds[2], bbox_preds[2] = ssd_forward_one(Xs[2], self.anchor_sizes[2],
+        anchors[2], cls_preds[2], bbox_pred[2] = ssd_forward_one(Xs[2], self.anchor_sizes[2],
                                                                  self.anchor_ratios[2], self.stage_2[0],
                                                                  self.stage_2[1])
         # print(X.shape)
-        anchors[3], cls_preds[3], bbox_preds[3] = ssd_forward_one(Xs[3],  self.anchor_sizes[3],
+        anchors[3], cls_preds[3], bbox_pred[3] = ssd_forward_one(Xs[3],  self.anchor_sizes[3],
                                                                  self.anchor_ratios[3], self.stage_3[0],
                                                                  self.stage_3[1])
         # print(X.shape)
-        anchors[4], cls_preds[4], bbox_preds[4] = ssd_forward_one(Xs[4], self.anchor_sizes[4],
+        anchors[4], cls_preds[4], bbox_pred[4] = ssd_forward_one(Xs[4], self.anchor_sizes[4],
                                                                  self.anchor_ratios[4], self.stage_4[0],
                                                                  self.stage_4[1])
         # print(X.shape)
-        anchors[5], cls_preds[5], bbox_preds[5] = ssd_forward_one(Xs[5],  self.anchor_sizes[5],
+        anchors[5], cls_preds[5], bbox_pred[5] = ssd_forward_one(Xs[5],  self.anchor_sizes[5],
                                                                  self.anchor_ratios[5], self.stage_5[0],  #last bug!!
                                                                  self.stage_5[1])
         # reshape函数中的0表示保持批量大小不变
         # print(X.shape)
-        return (F.concat(*anchors, dim=1), concat_preds(F,cls_preds).reshape((0, -1, self.num_classes + 1)),
-                concat_preds(F,bbox_preds))
-
+        anchors = F.concat(*anchors, dim=1)
+        cls_preds = F.reshape( concat_preds(F,cls_preds), (0, -1, self.num_classes + 1))
+        bbox_pred = concat_preds(F,bbox_pred)
+        if autograd.is_training():
+            return anchors, cls_preds, bbox_pred
+        return self.postprocess( anchors, cls_preds, bbox_pred )
+        #return anchors, cls_preds, bbox_pred
 
 
 if 0:
